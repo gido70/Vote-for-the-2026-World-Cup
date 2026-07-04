f35b8b4cd3037a887c1cf23734bdacf5322dabbf
gido70 <63196885+gido70@users.noreply.github.com>
2026-07-04 20:25:37 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
-// ضع بيانات Supabase هنا فقط.
-// Supabase Dashboard > Project Settings > API
 window.WC_SUPABASE_CONFIG = {
-  url: "PUT_SUPABASE_PROJECT_URL_HERE",
-  anonKey: "PUT_SUPABASE_ANON_PUBLIC_KEY_HERE",
+  url: "https://npoowwdjpbvhyjrmeqkv.supabase.co",
+  anonKey: "sb_publishable_VVHZURoVGvIV8Ys-lQOL5w_nR-Ayhw8",
   adminPin: "QX742"
 };
